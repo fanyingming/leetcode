35900e2d179cd3e18267c2d1ae3007b487ea616b
Yingming Fan <yingmingfan@gmail.com>
2015-08-09 19:15:10 +0800

    Create 33_Searchin_Rotated_Sorted_Array.c

---
+++ 33_Searchin_Rotated_Sorted_Array.c
@@ -0,0 +1,47 @@
+int binarySearch (int* nums, int numsSize, int target, int l, int r) {
+    int m;
+    
+    while (l <= r) {
+        m = (l+r)/2;
+        if (nums[m] < target)
+            l = m+1;
+        else if (nums[m] > target)
+            r = m-1;
+        else
+            return m;
+    }
+    return -1;
+}
+
+int findMin(int* nums, int numsSize) {
+    int min = nums[0];
+    int i, m;
+    int begin, end;
+    
+    begin = 0;
+    end = numsSize-1;
+    
+    //invariant: min value in [begin, end]
+    while (begin < end) {
+        m = (begin+end)/2;
+
+        if (nums[m] < nums[end])
+            end = m;
+        else if (nums[m] > nums[end])//这里m要加一，否则会有死循环。因为begin与m是可能重合的，此时会死循环。
+            begin = m+1;
+    }
+    return begin;
+}
+
+int search(int* nums, int numsSize, int target) {
+    int rv;
+    int index = findMin(nums, numsSize);
+    
+    if (target > nums[numsSize-1]) {//target bigger than most right num, then must appear in [0,index-1] or not appear at nums.
+        rv = binarySearch(nums, numsSize, target, 0, index-1);
+    } else {
+        rv = binarySearch(nums, numsSize, target, index, numsSize-1);
+    }
+    
+    return rv;
+}
