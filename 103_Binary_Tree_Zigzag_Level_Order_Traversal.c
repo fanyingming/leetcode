b740066570a271495cf7803f860f43bb961004af
Yingming Fan <yingmingfan@gmail.com>
2015-08-19 21:48:40 +0800

    Create 103_Binary_Tree_Zigzag_Level_Order_Traversal.c

---
+++ 103_Binary_Tree_Zigzag_Level_Order_Traversal.c
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    vector<vector<int>> rl;
+    queue<TreeNode*> que;
+    void BFS(TreeNode* root) {
+  //      que.clear();
+        que.push(root);
+        
+        int count = 1;
+        
+        while (que.empty() == false) {
+            int size = que.size();
+            vector<int> ver;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                ver.push_back(node->val);
+                if (node->left)
+                    que.push(node->left);
+                if (node->right)
+                    que.push(node->right);
+                que.pop();
+            }
+            if (count%2 == 1) {
+                rl.push_back(ver);
+            } else {
+                vector<int> reversed;
+                for (int i = ver.size()-1; i >= 0 ; i--)
+                    reversed.push_back(ver[i]);
+                rl.push_back(reversed);
+            }
+            count++;
+        }
+    }
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        rl.clear();
+        if (root == NULL)
+            return rl;
+        
+        BFS(root);
+        return rl;
+    }
+};
