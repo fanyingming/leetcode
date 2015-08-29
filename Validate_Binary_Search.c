a1412fbc05fe6b77303b994dd409e5b0e9cf7c46
Yingming Fan <yingmingfan@gmail.com>
2015-08-29 22:11:13 +0800

    Create Validate_Binary_Search.c

---
+++ Validate_Binary_Search.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include <limits.h>
+#include <stdint.h>
+bool recursive(struct TreeNode* root, int64_t min, int64_t max) {
+    bool bl, br;
+    bl = br = true;
+    if (!root) return true;
+    if (root->left) {
+        if (root->left->val >= root->val || root->left->val <= min || root->left->val >= max)
+            return false;
+
+        bl = recursive(root->left, min, root->val);
+        if (!bl)
+            return false;
+    }
+    if (root->right) {
+        if (root->right->val <= root->val || root->right->val <= min || root->right->val >= max)
+            return false;
+
+        br = recursive(root->right, root->val, max);
+    }
+    if (bl == true && br == true)
+        return true;
+    else
+        return false;
+}
+
+bool isValidBST(struct TreeNode* root) {
+    return recursive(root, LLONG_MIN, LLONG_MAX);
+}
