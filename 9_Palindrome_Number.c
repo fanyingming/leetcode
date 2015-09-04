0328bfc864136c1a4d26f93225dfd394591f98cb
fanyingming <yingmingfan@gmail.com>
2015-09-04 21:35:24 +0800

    we can try another mind, but haven't implemented.

--- 9_Palindrome_Number.c
+++ 9_Palindrome_Number.c
@@ -1,13 +1,20 @@
-bool isPalindrome(int x) {
+#include <limits.h>
+
+bool isPalindrome1(int x) {
     int reverse_num = 0;
     int x_t = x;
     
-    if (x < 0)
-        return false;
+    if (x < 0)	return false;
     
     while (x_t) {
+    	if (reverse_num > (INT_MAX-x_t%10)/10)
+    		return false;
         reverse_num = reverse_num*10 + x_t%10;
         x_t = x_t/10;
     }
     return reverse_num == x;
+}
+//another method is to compare first and last number.
+bool isPalindrome(int x) {
+
 }
