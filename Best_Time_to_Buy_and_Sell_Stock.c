dd4e21390de2922bd5eb177af977dd4c5aea6370
fanyingming <yingmingfan@gmail.com>
2015-09-11 23:09:31 +0800

    use more concise codes.

--- Best_Time_to_Buy_and_Sell_Stock.c
+++ Best_Time_to_Buy_and_Sell_Stock.c
@@ -1,23 +1,13 @@
 int maxProfit(int* prices, int pricesSize) {
     int i;
-    int min_index = 0;
     int max_profit = 0;
-    int max_index = 0;
-    int index_buy, index_sell;
+    int min_price = 0;
     
     if (prices == NULL || pricesSize < 2)   return 0;
     
     for (i = 0; i < pricesSize-1; i++) {
-        int profit;
-        if (prices[i] < prices[min_index])
-            min_index = i;
-        profit = prices[i+1] - prices[min_index];
-        if (profit > max_profit) {
-            max_profit = profit;
-            max_index = i+1;
-            index_buy = min_index;
-            index_sell = max_index;
-        }
+        min_price = min(prices[i], min_price);
+        max_profit= max(prices[i+1] - min_price, max_profit);
     }
     
     return max_profit;
