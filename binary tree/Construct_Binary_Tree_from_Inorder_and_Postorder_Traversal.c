f5a3c353b2c73a50f7be64438b2ffdd59b1f21ba
fanyingming <yingmingfan@gmail.com>
2015-09-17 22:49:58 +0800

    correct format.

--- binary tree/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.c
+++ binary tree/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.c
@@ -1,11 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+/*
+**采用如下的递归构建数，递归在从底至顶过程中，不断返回创建出的节点，以作为上一次的孩子节点。
+*/
 int index1;
 
 struct TreeNode* buildBT (int* postorder, int* inorder, int left, int right) {
@@ -21,7 +16,7 @@ struct TreeNode* buildBT (int* postorder, int* inorder, int left, int right) {
     node = malloc(sizeof(struct TreeNode));
     node->val = postorder[index1--];
     node->right = buildBT(postorder, inorder, i+1, right);
-    node->left= buildBT(postorder, inorder, left, i-1);
+    node->left  = buildBT(postorder, inorder, left, i-1);
     return node;
 }
 
