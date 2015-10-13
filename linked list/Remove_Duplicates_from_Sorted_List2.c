9ceeeea00ca50b700655052fcd9c2eeb5b7c5dbc
fanyingming <yingmingfan@gmail.com>
2015-10-13 14:59:48 +0800

    second time write codes.

--- linked list/Remove_Duplicates_from_Sorted_List2.c
+++ linked list/Remove_Duplicates_from_Sorted_List2.c
@@ -1,46 +1,33 @@
-
- // Definition for singly-linked list.
-struct ListNode {
-    int val;
-    struct ListNode *next;
-};
- #define NULL 0
 struct ListNode* deleteDuplicates(struct ListNode* head) {
-    struct ListNode prenode;
+    struct ListNode *p1;
+    struct ListNode *p2;
+    struct ListNode preNode;
     struct ListNode *pre;
-    struct ListNode *cur;
-    struct ListNode *next;
-    int b_remove = 0;
-    
-    pre = &prenode;
-    prenode.next = head;
-    
-    if (head == NULL || head->next == NULL)
-        return head;
-    
-    cur = pre->next;
-    next= cur->next;
-    
-    //invariant: nodes before cur no dumlicates
-    while (next) {
-        if (cur->val == next->val) {
-            b_remove++;
-            next = next->next;
+    bool b_same = false;
+
+    if (head == NULL || head->next == NULL) return head;
+    preNode.next = head;
+    pre = &preNode;
+    p1 = head;
+    p2 = head->next;
+    while (p2 != NULL) {
+        if (p1->val == p2->val) {
+            b_same = true;
+            p2 = p2->next;
         } else {
-            if (b_remove) {
-                pre->next = next;
-                cur = next;
-                next = next->next;
+            if (b_same) {
+                pre->next = p2;
+                p1 = p2;
+                p2 = p1->next;
+                b_same = false;
             } else {
-                pre = cur;
-                cur = next;
-                next = next->next;
+                pre = pre->next;
+                p1 = p1->next;
+                p2 = p2->next;
             }
-            b_remove = 0;
         }
     }
-    if (b_remove) {
-        pre->next = next;
-    }
-    return prenode.next;
+    if (b_same)
+        pre->next = NULL;
+    return preNode.next;
 }
